b87d43f38bcc2059f3e8cbf42adb939ecb43ad3f
Amaresh Kumar <amareshkumar7525@gmail.com>
2025-10-21 13:21:34 +0530

    Added: A Simple Banking_Smart_Contract.sol
    Updated comments for clarity and consistency.

--- Banking_Smart_Contract.sol
+++ Banking_Smart_Contract.sol
@@ -2,40 +2,46 @@
 pragma solidity ^0.8.0;
 
 contract BankSmartContract {
-    mapping(address => uint) public balances; //mapping for check balance
+    // Mapping to store user balances
+    mapping(address => uint) public balances;
 
-    //some event for tracking history
+    // Events for tracking history
     event TrackDeposit(address indexed sender, uint amount);
     event TrackWithdraw(address indexed receiver, uint amount);
     event TrackTransfer(address indexed from, address indexed to, uint amount);
 
+    // Modifier to check sufficient balance
     modifier CheckBalance(uint amount) {
         require(balances[msg.sender] >= amount, "Insufficient Balance!");
         _;
     }
 
-    //deposit fund
+    // Deposit funds into the contract
     function deposit() public payable {
         balances[msg.sender] += msg.value;
         emit TrackDeposit(msg.sender, msg.value);
     }
 
-    //withdraw balance into your wallet
+    // Withdraw funds from the contract
     function withdraw(uint amount) public CheckBalance(amount) {
         balances[msg.sender] -= amount;
         payable(msg.sender).transfer(amount);
         emit TrackWithdraw(msg.sender, amount);
     }
 
-    //transfer blances user to user
+    // Transfer balance from sender to another user
     function transfer(address user, uint amount) public CheckBalance(amount) {
         balances[msg.sender] -= amount;
         balances[user] += amount;
         emit TrackTransfer(msg.sender, user, amount);
     }
 
-    //checks the balance of this contract
+    // Get total ETH stored in this contract
     function getContractBalance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    function getMyBalance() public view returns (uint) {
+        return balances[msg.sender];
     }
 }
